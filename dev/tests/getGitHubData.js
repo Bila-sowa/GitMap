f836fa4be77e1b500e703245df0ee7a27124445d
Bila_sowa <zlepkopavlo9@gmail.com>
2026-07-26 14:02:33 +0300

    update: getGitHubData.js

--- dev/tests/getGitHubData.js
+++ dev/tests/getGitHubData.js
@@ -1,22 +1,51 @@
-async function getGitHubData(url) {
-    const formattedUrl = `${url}`
+class GitHubClient {
+    constructor(url) {
+        const { branchLink, commitsLink } = GitHubClient.formatGitHubUrl(url);
+        this.branchLink = branchLink;
+        this.commitsLink = commitsLink;
+        this.headers = { 'Accept': 'application/vnd.github+json', };
+    }
 
-    const headers = {
-        'Accept': 'application/vnd.github+json',
+    static formatGitHubUrl(url) {
+        const match = url.match(/github\.com\/([^/]+)\/([^/]+)/);
+        if (!match) throw new Error('Invalid GitHub URL');
+
+        const [, owner, repo] = match;
+        const cleanRepo = repo.replace(/\.git$/, '').replace(/\/$/, '');
+
+        return {
+            branchLink: `https://api.github.com/repos/${owner}/${cleanRepo}/branches`,
+            commitsLink: `https://api.github.com/repos/${owner}/${cleanRepo}/commits`,
+        };
     };
 
-    const branchesRes = await fetch(
-        "https://api.github.com/repos/Bila-sowa/GitMap/branches",
-        { headers }
-    );
+    static async getRawData() {
+        const branchesRes = await fetch(this.branchLink, { headers: this.headers });
+        const commitsRes = await fetch(this.commitsLink, { headers: this.headers });
 
-    const commitsRes = await fetch(
-        "https://api.github.com/repos/Bila-sowa/GitMap/commits",
-        { headers }
-    );
+        const branches = await branchesRes.json();
+        const commits = await commitsRes.json();
+        return { branches, commits };
+    };
 
-    const branches = await branchesRes.json();
-    const commits = await commitsRes.json();
-    return { branches, commits };
+    async getData () {
+        const { branches, commits } = await this.getRawData();
+        let commitsDetails = [];
+
+        commits.forEach(commit => {
+            const details = {
+                author: commit.commit.author,
+                message: commit.commit.message,
+                url: commit.commit.url,
+            };
+            
+            commitsDetails.push(details);
+        });
+
+        return {
+            commitsDetails: commitsDetails,
+        };
+    };
 }
 
+const client = new GitHubClient("https://github.com/Bila-sowa/Web-player-");
