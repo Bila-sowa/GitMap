2c8cac53cae0eeed2ae34889154b179625a78d3f
Bila_sowa <zlepkopavlo9@gmail.com>
2026-07-30 16:32:41 +0300

    done: header

---
+++ src/js/utils/a11y.js
@@ -0,0 +1,15 @@
+// const navButtons = document.querySelectorAll('.nav-button');
+// const burgerMenuContainer = document.querySelector('.burger-menu-content');
+// const headerNav = document.querySelector('.header-nav');
+
+// const mq = window.matchMedia('(max-width: 600px)');
+
+// function moveButtons(e) {
+//     if (e.matches) {
+//         navButtons.forEach(btn => burgerMenuContainer.appendChild(btn));
+//     } else {
+//         navButtons.forEach(btn => headerNav.appendChild(btn));
+//     }
+// }
+
+// mq.addEventListener('change', moveButtons);
