7789615e8261b950d53e43b5edb4c02b4527acbc
Bila_sowa <zlepkopavlo9@gmail.com>
2026-08-02 15:54:35 +0300

    update: graph.js

--- src/js/controllers/graph.js
+++ src/js/controllers/graph.js
@@ -3,21 +3,26 @@ import GitHubClient from "../services/getGitHubData.js";
 export default class Graph {
     #graph;
     #client;
-    #commitsDetails = [];
+    #data;
+    #cooldown;
+    #isOpen;
+    #hoverTimeoutId = null;
 
     constructor(graphElement) {
         this.#graph = graphElement;
+        this.#cooldown = 1500;
         this.#bindEvents();
     }
 
-    async render (link) {
+    async render(link) {
         if (!link) return;
-        const client = new GitHubClient(link);
-        const data = await client.getData();
 
-        data.commitsDetails.forEach((commit, index) => {
+        await this.#getData(link);
+        this.#graph.innerHTML = '';
+
+        this.#data.commitsDetails.forEach((commit, index) => {
             const formattedTitle = commit.message.split('\n')[0];
-            const isLast = index === data.commitsDetails.length - 1;
+            const isLast = index === this.#data.commitsDetails.length - 1;
 
             const commitCard = `
                 <button class="commit" data-id="${index}" aria-label="Open commit ${formattedTitle}" title="Open commit ${formattedTitle}"></button>
@@ -29,9 +34,9 @@ export default class Graph {
                 `}
             `;
 
-            graph.insertAdjacentHTML("beforeend", commitCard);
+            this.#graph.insertAdjacentHTML("beforeend", commitCard);
         });
-    };
+    }
 
     #bindEvents() {
         this.#graph.addEventListener('click', this.#handleCommitClick);
@@ -39,40 +44,60 @@ export default class Graph {
         this.#graph.addEventListener('mouseout', this.#handleCommitLeave);
     }
 
-    #handleCommitClick = (event) => {
-        const commitButton = event.target.closest('.commit');
-        if (!commitButton) return;
-
-        const commitIndex = commitButton.dataset.id;
-        console.log('Відкрито коміт з індексом:', commitIndex);
-        // тут логіка відкриття коміту
-    };
-
-    #handleCommitHover = (event) => {
-        const commitButton = event.target.closest('.commit');
-        if (!commitButton) return;
-
-        const commitIndex = Number(commitButton.dataset.id);
-        this.openCommitModal(commitIndex);
-    };
-
-    #handleCommitLeave = (event) => {
-        const commitButton = event.target.closest('.commit');
-        if (!commitButton) return;
-
-        this.closeCommitModal();
-    };
-
-    openCommitModal(commitIndex) {
-        const commit = this.#commitsDetails[commitIndex];
-        if (!commit) return;
-
-        console.log('Показати модальне вікно для коміту:', commit);
-        // тут логіка показу модального вікна
+    async #getData(link) {
+        this.#client = new GitHubClient(link);
+        this.#data = await this.#client.getData();
     }
 
-    closeCommitModal() {
-        console.log('Приховати модальне вікно');
-        // тут логіка приховування модального вікна
+    #handleCommitClick = (e) => {
+        const commitButton = e.target.closest('.commit');
+        if (!commitButton) return;
+
+        const commitIndex = +commitButton.dataset.id;
+        this.#openFullCommitModal(commitIndex);
+    };
+
+    #handleCommitHover = (e) => {
+        const commitButton = e.target.closest('.commit');
+        if (!commitButton || this.#isOpen) return;
+
+        const commitIndex = +commitButton.dataset.id;
+
+        this.#hoverTimeoutId = setTimeout(() => {
+            this.#openHoverCommitModal(commitIndex);
+            this.#hoverTimeoutId = null;
+        }, this.#cooldown);
+    };
+
+    #handleCommitLeave = (e) => {
+        const commitButton = e.target.closest('.commit');
+        if (!commitButton) return;
+
+        if (this.#hoverTimeoutId) {
+            clearTimeout(this.#hoverTimeoutId);
+            this.#hoverTimeoutId = null;
+        }
+
+        if (this.#isOpen) {
+            this.#closeCommitModal();
+        }
+    };
+
+    #openFullCommitModal = (commitIndex) => {
+        const commit = this.#data?.commitsDetails[commitIndex];
+        if (!commit || this.#isOpen) return;
+
+        this.#isOpen = true;
+    }
+
+    #openHoverCommitModal = (commitIndex) => {
+        const commit = this.#data?.commitsDetails[commitIndex];
+        if (!commit || this.#isOpen) return;
+
+        this.#isOpen = true;
+    }
+
+    #closeCommitModal = () => {
+        this.#isOpen = false;
     }
 }
