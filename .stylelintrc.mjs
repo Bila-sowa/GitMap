f498f675be1c6cfde5a576f7ae69ffb05583dfc0
Bila_sowa <zlepkopavlo9@gmail.com>
2026-08-28 15:15:33 +0300

    fixed: prettier, stylelint, imports in the index.js

--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -1,6 +1,12 @@
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('stylelint').Config} */
 export default {
     extends: ["stylelint-config-standard-scss"],
+    configBasedir: __dirname,
     rules: {
         "declaration-block-no-duplicate-custom-properties": true,
         "declaration-block-no-duplicate-properties": true,
@@ -14,5 +20,5 @@ export default {
         "function-calc-no-unspaced-operator": true,
         "no-invalid-position-declaration": true,
         "alpha-value-notation": "percentage",
-    }
+    },
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,9 @@
 import "../styles.scss";
+import "./controllers/localStorage.js";
 import "./controllers/canvas.js";
 import "./controllers/graph.js";
 import "./controllers/link.js";
-import "./controllers/localStorage.js";
 import "./controllers/refresh.js";
 import "./controllers/settings.js";
 import "./controllers/theme.js";
+
